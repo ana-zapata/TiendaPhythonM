517de1388974e4214ee233ef103d24f83d72b4a4
ana-zapata <anamariazapata@2004Qgmail.com>
2025-02-25 20:59:51 -0500

    feat se crea menu de opciones en el main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #software estructuras
 nombreVendedor="Ana Maria"
+productos=[]
+producto={}
+
+#CREANDO UN CICLO EN PYTHON
+opcion=None
+print("Merqueo App")
+print("1. Crear lista de mercado")
+print("5. Salir")
+
+while opcion != 5:
+    print("estoy adentro")
+    opcion=int(input("Digita una opcion: "))
