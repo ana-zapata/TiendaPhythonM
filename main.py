55cab987d85779c4943fa3a097d7e1aab5e134bb
ana-zapata <anamariazapata@2004Qgmail.com>
2025-03-04 20:57:18 -0500

    se crea opcion 1 para poblar diccionario y lista de productos

--- main.py
+++ main.py
@@ -4,11 +4,38 @@ productos=[]
 producto={}
 
 #CREANDO UN CICLO EN PYTHON
-opcion=None
+opcion=100
 print("Merqueo App")
 print("1. Crear lista de mercado")
+print("2. Ver lista de mercado")
+print("3. Editar producto de la lista")
+print("4. Retirar producto de la lista")
 print("5. Salir")
 
 while opcion != 5:
     print("estoy adentro")
     opcion=int(input("Digita una opcion: "))
+    if opcion == 1:
+        print("Bienvenido a la creacion de tu lista de mercado")
+        #CREANDO CLAVES Y VALORES DE UN DICCIONARIO
+        producto["id"]=5
+        producto["nombre"]=input("Digita el nombre del producto: ")
+        producto["precio"]=int(input("Digita el precio del producto: "))
+        producto["cantidad"]=int(input("Cuantos elementos de este producto vas a llevar: "))
+        producto["presentacion"]=input("¿Cual presentacion deseas? ")
+
+        #MOSTRANDO MI DICCIONARIO
+        #print(producto)
+
+        #POBLANDO UNA LISTA (AGREGANDO ELEMENTOS A UNA LISTA), LLAMAR LA LISTA SI ESTA EN PLURAL ES UNA LISTA, SI ESTA EN SINGULAR ES DICCIONARIO
+        productos.append(producto) ##AGREGAR ELEMENTOS A LA LISTA
+        print(productos)
+
+    elif opcion == 2:
+        print("Estoy en la dos")
+    elif opcion == 3:
+        print("Estoy en la tres")
+    elif opcion == 4:
+        print("Estoy en la cuatro")
+    else:
+        print("Opcion invalida")
